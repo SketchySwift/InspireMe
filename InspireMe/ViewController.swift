3cac9f5f3e0c60646bce6414b45ba6872249cfbb
HardcoreIOS <Abdurrahman@owners-mbp.home>
2015-12-03 07:38:47 -0500

    Initial Commit

---
+++ InspireMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InspireMe
+//
+//  Created by Abdurrahman on 12/3/15.
+//  Copyright © 2015 Hafiz Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
